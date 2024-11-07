d87e3ff0e54c874d140093f416fc5a7112fc7e12
alejandro950825 <alejandro950825@gmail.com>
2024-11-07 17:56:16 -0600

    Deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
 })
